b58c922a487386abe80822e20a2a094f3726520d
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2019-05-30 15:15:46 -0700

    Bitcoin benchmarks, under version control

---
+++ tests/unit/out/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+KREMLIN_HOME=/home/jonathan/Code/kremlin
+PATH=/home/jonathan/opt/compcert/bin:$PATH
+
+for c in "ccomp -fstruct-passing" gcc-7 "gcc-7 -O2" "gcc-7 -O3" gcc-8 "gcc-8 -O2" "gcc-8 -O3" clang-7 "clang-7 -O2" "clang-7 -O3" clang-8 "clang-8 -O2" "clang-8 -O3"; do
+  rm -rf a.out *.o
+  $c -I $KREMLIN_HOME/include -I . -DKRML_VERIFIED_UINT128 -D_DEFAULT_SOURCE *.c $KREMLIN_HOME/kremlib/dist/generic/libkremlib.a &> log
+  echo $c && ./a.out
+done
+
+chmod a+rw log
+rm -rf a.out
