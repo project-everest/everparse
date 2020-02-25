c9d1d1db3615cd068e21edc64c56b19bb5ea55c6
Nikhil Swamy <nikswamy@users.noreply.github.com>
2020-02-25 11:08:14 -0800

    Module-specific error reporting functions and callback stub

--- src/3d/EverParseError.h
+++ src/3d/EverParseError.h
@@ -18,5 +18,16 @@ nswamy, protz, taramana 5-Feb-2020
 --*/
 #ifndef __EverParseError_H
 #define ___EverParseError_H
-void EverParseError(char *x, char *y, char *z);
+static char* EverParseErrorReasonOfResult (uint64_t code)
+{
+    switch (EverParseErrorKindOfResult(code)) {
+        case 1: return "generic error";
+        case 2: return "not enough data";
+        case 3: return "impossible";
+        case 4: return "list size not multiple of element size";
+        case 5: return "action failed";
+        case 6: return "constraint failed";
+        default: return "unspecified";
+    }
+}
 #endif
