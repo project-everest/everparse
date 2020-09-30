b826da34bd2bf568e7c3e50a7512043b8f550f36
Tahina Ramananandro <taramana@microsoft.com>
2020-09-30 08:54:09 -0700

    test_mitls_parsers: do not build 3d

--- .docker/build/build.sh
+++ .docker/build/build.sh
@@ -122,7 +122,7 @@ function rebuild_doc () {
 function test_mitls_parsers () {
     if [[ "$branchname" == "master" ]] ; then
         fetch_and_make_kremlin &&
-        OTHERFLAGS='--admit_smt_queries true' make -j $threads &&
+        OTHERFLAGS='--admit_smt_queries true' make -j $threads quackyducky lowparse &&
         export_home QD "$(pwd)" &&
         fetch_mitls &&
         make -j $threads -C $MITLS_HOME/src/parsers verify
