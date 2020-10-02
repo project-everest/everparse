bfbfb328013e92d1153997c20f40df22896caa8e
Tahina Ramananandro <taramana@microsoft.com>
2020-10-01 18:16:44 -0700

    c3d/build.sh: allow an argument to tag the build image and container

--- c3d/build.sh
+++ c3d/build.sh
@@ -5,7 +5,17 @@
 
 set -e
 set -x
-docker build -t build-c3d -f package/Dockerfile .
-docker create --name=build-c3d build-c3d
-docker cp build-c3d:/home/test/out/C3d.so .
-docker rm build-c3d
+
+build_tag=$1
+if [[ -z $build_tag ]] ; then
+    build_image=build-c3d
+else
+    build_image=build-c3d:$build_tag
+fi
+docker build -t $build_image -f package/Dockerfile .
+docker create --name=build-c3d-$build_tag $build_image
+# The plugin is useful for those who already have clang installed and wish to
+# run it as part of a global compilation run; the executable is likely more
+# useful.
+docker cp build-c3d-$build_tag:/home/test/out/C3d.so .
+docker rm build-c3d-$build_tag
