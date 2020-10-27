b38e4c1ab775c7f56e34288f837e9bf284452023
Jonathan Protzenko <protz@microsoft.com>
2020-10-27 16:35:22 -0700

    Joy and fulfillment: deal with xcrun on OSX to figure out the sysroot, wrangle with clang's option framework, figure out how to drive the debug framework without the suitable options being passed, discover that the CompilationDatabase won't return a valid list of files, etc. etc. etc.

--- c3d/driver/main.cpp
+++ c3d/driver/main.cpp
@@ -1,8 +1,10 @@
+#include "clang/Driver/Compilation.h"
 #include "clang/Tooling/Tooling.h"
 #include "clang/Frontend/FrontendActions.h"
 #include "clang/Tooling/CommonOptionsParser.h"
-#include "llvm/Support/Debug.h"
-#include <llvm/Support/CommandLine.h>
+#include "llvm/Support/Program.h"
+#include "llvm/Support/FileUtilities.h"
+#include "llvm/Support/CommandLine.h"
 
 #include "C3d.h"
 
@@ -22,59 +24,108 @@ public:
 
   std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
                                                  StringRef file) override {
+    LLVM_DEBUG(llvm::dbgs() << "clang-c3d: start AST consumer\n");
     return std::make_unique<C3dASTConsumer>(&CI);
   }
 };
 
-int main(int argc, const char **argv) {
+// Option to specify a file name for a list of header files to check.
+static cl::list<std::string>
+    ListFileNames(cl::Positional, cl::value_desc("list"),
+                  cl::desc("<list of one or more header list files>"),
+                  cl::CommaSeparated);
+
+static cl::opt<bool>
+Debug("c3debug", cl::init(false),
+cl::desc("Enabled c3d internal debugging"));
+
+
+// Collect all other arguments, which will be passed to the front end.
+static cl::list<std::string>
+    CC1Arguments(cl::ConsumeAfter,
+                 cl::desc("<arguments to be passed to front end>..."));
+
+// Query apple's `xcrun` launcher, which is the source of truth for "how should"
+// clang be invoked on this system.
+llvm::Optional<std::string> queryXcrun(llvm::ArrayRef<llvm::StringRef> Argv) {
+  auto Xcrun = llvm::sys::findProgramByName("xcrun");
+  if (!Xcrun) {
+    fprintf(stderr, "Couldn't find xcrun. Hopefully you have a non-apple toolchain...");
+    return llvm::None;
+  }
+  llvm::SmallString<64> OutFile;
+  llvm::sys::fs::createTemporaryFile("clangd-xcrun", "", OutFile);
+  llvm::FileRemover OutRemover(OutFile);
+  llvm::Optional<llvm::StringRef> Redirects[3] = {
+      /*stdin=*/{""}, /*stdout=*/{OutFile}, /*stderr=*/{""}};
+  // vlog("Invoking {0} to find clang installation", *Xcrun);
+  int Ret = llvm::sys::ExecuteAndWait(*Xcrun, Argv,
+                                      /*Env=*/llvm::None, Redirects,
+                                      /*SecondsToWait=*/10);
+  if (Ret != 0) {
+    fprintf(stderr, "xcrun exists but failed with code %d. "
+        "If you have a non-apple toolchain, this is OK. "
+        "Otherwise, try xcode-select --install.",
+        Ret);
+    return llvm::None;
+  }
+
+  auto Buf = llvm::MemoryBuffer::getFile(OutFile);
+  if (!Buf) {
+    fprintf(stderr, "Can't read xcrun output: %s", Buf.getError().message().c_str());
+    return llvm::None;
+  }
+  StringRef Path = Buf->get()->getBuffer().trim();
+  if (Path.empty()) {
+    fprintf(stderr, "xcrun produced no output");
+    return llvm::None;
+  }
+  return Path.str();
+}
+
+int main(int Argc, const char *Argv[]) {
   // TODO: the following steps need to happen
-  // i) figure out how to pass suitable options to the compiler invocations,
-  //    e.g. how to pass -std=c2x, how to pass a proper -include, etc.
   // ii) figure out how to load and enable plugins; apparently the plugin
   //     loading code does not kick in with our custom driver, but! we are
   //     linking in the plugin so it ought to do something (famous last words?)
 
-  // Build a modified command-line where the arguments below have been prepended
-  // to the invocation.
-  int MyArgc = argc + 3;
-  const char *MyArgv[MyArgc];
-  MyArgv[0] = argv[0];
-  MyArgv[1] = "-std=c2x";
-  MyArgv[2] = "-mllvm";
-  MyArgv[3] = "-debug-only=c3d";
-  memcpy(MyArgv+4, argv+1, (argc - 1)*sizeof(char *));
+  llvm::Optional<std::string> Path;
+  SmallVector<const char *, 20> NewArgv;
+  NewArgv.push_back(Argv[0]);
+  for (int i = 1; i < Argc; ++i)
+    NewArgv.push_back(Argv[i]);
+  NewArgv.push_back("-std=c2x");
+#ifdef __APPLE__
+  Path = queryXcrun({"xcrun", "--show-sdk-path"});
+  if (Path.hasValue()) {
+    NewArgv.push_back("-isysroot");
+    NewArgv.push_back(Path.getValue().c_str());
+  }
+#endif
 
-  cl::ParseCommandLineOptions(MyArgc, MyArgv, "c3d.\n");
+  int NewArgc = static_cast<int>(NewArgv.size());
+  cl::ParseCommandLineOptions(NewArgc, &NewArgv[0], "c3d.\n");
 
-
-  /* for (int i = 0; i < MyArgc; ++i) */
-  /*   printf("%s ", MyArgv[i]); */
-  /* printf("\n"); */
-  std::string ErrorMsg;
-  std::unique_ptr<CompilationDatabase> MyCD;
-  MyCD.reset(
-    FixedCompilationDatabase::loadFromCommandLine(MyArgc, MyArgv, ErrorMsg));
-  if (MyCD == nullptr) {
-    fprintf(stderr, "Error parsing the command-line:\n%s\n", ErrorMsg.c_str());
-    exit(1);
+  // After this line, LLVM_DEBUG will start doing something!
+  if (Debug.getValue()) {
+    llvm::DebugFlag = true;
+    setCurrentDebugType(DEBUG_TYPE);
   }
 
-  printf("all compile commands (%d):\n", MyArgc);
-  for (auto &CC: MyCD->getAllCompileCommands()) {
-    printf("invocation: ");
-    for (const auto &s: CC.CommandLine)
-      printf("%s ", s.c_str());
-    printf("\n");
+  // No go if we have no header list file.
+  if (ListFileNames.size() == 0) {
+    cl::PrintHelpMessage();
+    return 1;
   }
 
-  // TODO: OP.getCompilations() is useless as it attempts to read from some
-  // CompileCommands!
-  ClangTool Tool(*MyCD, MyCD->getAllFiles());
+  SmallString<256> PathBuf;
+  sys::fs::current_path(PathBuf);
+  std::unique_ptr<CompilationDatabase> Compilations;
+  Compilations.reset(
+      new FixedCompilationDatabase(Twine(PathBuf), CC1Arguments));
 
-  // ClangTool::run accepts a FrontendActionFactory, which is then used to
-  // create new objects implementing the FrontendAction interface. Here we use
-  // the helper newFrontendActionFactory to create a default factory that will
-  // return a new MyFrontendAction object every time.
-  // To further customize this, we could create our own factory class.
+  // Compilations.getFileNames() doesn't work here.
+  ClangTool Tool(*Compilations, ListFileNames);
+
   return Tool.run(newFrontendActionFactory<C3dFrontendAction>().get());
 }
