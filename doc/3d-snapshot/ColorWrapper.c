ba4d469f52114256c5877f3184903c5ff01e2dcb
Tahina Ramananandro <taramana@microsoft.com>
2021-02-24 17:23:48 -0800

    doc snap ???

--- doc/3d-snapshot/ColorWrapper.c
+++ doc/3d-snapshot/ColorWrapper.c
@@ -4,44 +4,50 @@
 void ColorEverParseError(char *x, char *y, char *z);
 static char* ColorStructNameOfErr(uint64_t err) {
 	switch (EverParseFieldIdOfResult(err)) {
-		case 1: return "ColoredPoint._point";
-		case 2: return "ColoredPoint._point";
-		case 3: return "ColoredPoint._coloredPoint1";
-		case 4: return "ColoredPoint._coloredPoint2";
+		case 1: return "BoundedSum._boundedSum";
+		case 2: return "BoundedSum._boundedSum";
+		case 3: return "BoundedSum.mySum";
+		case 4: return "Triangle._point";
 		case 5: return "Triangle._point";
-		case 6: return "Triangle._point";
+		case 6: return "OrderedPair._orderedPair";
 		case 7: return "OrderedPair._orderedPair";
-		case 8: return "OrderedPair._orderedPair";
+		case 8: return "HelloWorld._point";
 		case 9: return "HelloWorld._point";
-		case 10: return "HelloWorld._point";
-		case 11: return "Triangle2._point";
-		case 12: return "Triangle2._point";
-		case 13: return "Triangle2._triangle";
-		case 14: return "Color._coloredPoint";
-		case 15: return "Color._coloredPoint";
-		case 16: return "Color._coloredPoint"; 
+		case 10: return "ColoredPoint._point";
+		case 11: return "ColoredPoint._point";
+		case 12: return "ColoredPoint._coloredPoint1";
+		case 13: return "ColoredPoint._coloredPoint2";
+		case 14: return "Triangle2._point";
+		case 15: return "Triangle2._point";
+		case 16: return "Triangle2._triangle";
+		case 17: return "Color._coloredPoint";
+		case 18: return "Color._coloredPoint";
+		case 19: return "Color._coloredPoint"; 
 		default: return "";
 	}
 }
 
 static char* ColorFieldNameOfErr(uint64_t err) {
 	switch (EverParseFieldIdOfResult(err)) {
-		case 1: return "x";
-		case 2: return "y";
-		case 3: return "color";
-		case 4: return "color";
-		case 5: return "x";
-		case 6: return "y";
-		case 7: return "lesser";
-		case 8: return "greater";
-		case 9: return "x";
-		case 10: return "y";
-		case 11: return "x";
-		case 12: return "y";
-		case 13: return "corners";
-		case 14: return "col";
-		case 15: return "x";
-		case 16: return "y"; 
+		case 1: return "left";
+		case 2: return "right";
+		case 3: return "bound";
+		case 4: return "x";
+		case 5: return "y";
+		case 6: return "lesser";
+		case 7: return "greater";
+		case 8: return "x";
+		case 9: return "y";
+		case 10: return "x";
+		case 11: return "y";
+		case 12: return "color";
+		case 13: return "color";
+		case 14: return "x";
+		case 15: return "y";
+		case 16: return "corners";
+		case 17: return "col";
+		case 18: return "x";
+		case 19: return "y"; 
 		default: return "";
 	}
 }
--- doc/3d-snapshot/OrderedPair.c
+++ doc/3d-snapshot/OrderedPair.c
@@ -5,12 +5,12 @@
 /*
 Auto-generated field identifier for error reporting
 */
-#define ORDEREDPAIR__ORDEREDPAIR__LESSER ((uint64_t)7U)
+#define ORDEREDPAIR__ORDEREDPAIR__LESSER ((uint64_t)6U)
 
 /*
 Auto-generated field identifier for error reporting
 */
-#define ORDEREDPAIR__ORDEREDPAIR__GREATER ((uint64_t)8U)
+#define ORDEREDPAIR__ORDEREDPAIR__GREATER ((uint64_t)7U)
 
 static inline uint64_t ValidateOrderedPairLesser(uint32_t InputLength, uint64_t StartPosition)
 /*++
