63b27f0cc90bb73be518f28505efbc7f6f5ffc7c
Tahina Ramananandro <taramana@microsoft.com>
2021-06-30 08:30:38 -0700

    update to the latest HACL* OCaml API (cf. project-everest/hacl-star#450)

--- src/3d/ocaml/evercrypt/HashingBase.ml
+++ src/3d/ocaml/evercrypt/HashingBase.ml
@@ -1,11 +1,8 @@
 type t = Hacl_star__EverCrypt.Hash.t
 let alg = Hacl_star__SharedDefs.HashDefs.SHA2_256
-let hlen = Hacl_star__SharedDefs.HashDefs.digest_len alg
 
-let init () = Hacl_star__EverCrypt.Hash.init alg
-let update = Hacl_star__EverCrypt.Hash.update
+let init () = Hacl_star__EverCrypt.Hash.init ~alg:alg
+let update st data = Hacl_star__EverCrypt.Hash.update ~st:st ~msg:data
 
 let finish h =
-  let buf = Bytes.create hlen in
-  Hacl_star__EverCrypt.Hash.finish h buf;
-  buf
+  Hacl_star__EverCrypt.Hash.finish ~st:h
