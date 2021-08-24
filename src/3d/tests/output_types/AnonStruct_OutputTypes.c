e9cfe575c6366434ec0692e993d98d0e04a5ba35
Aseem Rastogi <aseemr@microsoft.com>
2021-08-24 22:51:15 +0530

    C files too

---
+++ src/3d/tests/output_types/AnonStruct_OutputTypes.c
@@ -0,0 +1,25 @@
+
+
+typedef _OTPOINT* POtpoint;
+
+
+
+static inline void SetOtpointDerefX (_OTPOINT* out, uint8_t __v){
+    (out)->(x) = __v;;
+}
+
+
+
+
+static inline void SetOtpointDerefY (_OTPOINT* out, uint8_t __v){
+    (out)->(y) = __v;;
+}
+
+
+
+
+static inline void SetOtpointDerefZ (_OTPOINT* out, uint8_t __v){
+    (out)->(z) = __v;;
+}
+
+
---
+++ src/3d/tests/output_types/AnonUnion_OutputTypes.c
@@ -0,0 +1,25 @@
+
+
+typedef _OPOINT* POpoint;
+
+
+
+static inline void SetOpointDerefZ (_OPOINT* out, uint32_t __v){
+    (out)->(z) = __v;;
+}
+
+
+
+
+static inline void SetOpointDerefY (_OPOINT* out, uint8_t __v){
+    (out)->(y) = __v;;
+}
+
+
+
+
+static inline void SetOpointDerefX (_OPOINT* out, uint8_t __v){
+    (out)->(x) = __v;;
+}
+
+
