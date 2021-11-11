f8d4a460f0b65272c7cde42a3e2f44c746fd15c8
Aseem Rastogi <aseemr@microsoft.com>
2021-11-11 18:19:14 +0000

    extern vector example

---
+++ src/3d/tests/output_types/ExternVectorDriver.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include "ExternVector_ExternalTypedefs.h"
+#include "ExternVectorWrapper.h"
+
+void ExternVectorEverParseError(const char *StructName, const char *FieldName, const char *Reason)
+{
+  return;
+}
+
+void Push (Vec *vec, uint32_t x, uint32_t y)
+{
+  if(vec->max == vec->cur) {
+    return;
+  } else {
+    (vec->arr[vec->cur]).x = x;
+    (vec->arr[vec->cur]).y = y;
+    vec->cur = vec->cur+1;    
+  }
+}
+
+Vec *Alloc ()
+{
+  Vec *vec = (Vec *) malloc(sizeof(Vec));
+  vec->max = 2;
+  vec->cur = 0;
+  vec->arr = (Point *) malloc(sizeof(Point) * 2);
+  return vec;
+}
+
+int main ()
+{
+  uint8_t *data = malloc(sizeof(uint8_t) + sizeof(uint32_t) * 6);
+  data[0] = 3;
+  uint32_t *arr = (uint32_t *) (data + 1);
+  arr[0] = 0;
+  arr[1] = 1;
+  arr[2] = 2;
+  arr[3] = 3;
+  arr[4] = 4;
+  arr[5] = 5;
+
+  Vec *vec = Alloc();
+  
+  BOOLEAN b = ExternVectorCheckT(vec, (uint8_t *) data, sizeof(uint8_t) + sizeof(uint32_t) * 6);
+  if(b &&
+     vec->cur == 2      &&
+     vec->arr[0].x == 0 &&
+     vec->arr[0].y == 1 &&
+     vec->arr[1].x == 2 &&
+     vec->arr[1].y == 3) {
+    printf("%s\n", "Validation succeeded");
+    return 0;
+  } else {
+    printf("%s\n", "Validation failed");
+    return 1;
+  }
+}
