b4f45a323af10add131b616ee6c54284a5c9f73c
Tahina Ramananandro <taramana@microsoft.com>
2022-02-23 17:25:31 -0800

    update tcpip extern test

--- src/3d/tests/tcpip/extern/EverParseStream.h
+++ src/3d/tests/tcpip/extern/EverParseStream.h
@@ -21,4 +21,10 @@ EverParseInputStreamBase EverParseCreate();
 
 int EverParsePush(EverParseInputStreamBase x, uint8_t * buf, uint64_t len);
 
+// dummy types, they are not used
+typedef int EverParseHasT;
+typedef int EverParseReadT;
+typedef int EverParseSkipT;
+typedef int EverParseEmptyT;
+
 #endif // __EVERPARSESTREAM
