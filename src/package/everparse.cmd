c7a1f653f7cc50a5e5c2347ec08ee71678954934
Tahina Ramananandro <taramana@microsoft.com>
2022-03-25 15:49:42 -0700

    krml rename: package

--- src/package/everparse.cmd
+++ src/package/everparse.cmd
@@ -3,6 +3,6 @@ setlocal
 set mypath0=%~dp0
 set QD_HOME=%mypath0:~0,-1%
 set FSTAR_HOME=%QD_HOME%
-set KREMLIN_HOME=%QD_HOME%
+set KRML_HOME=%QD_HOME%
 %QD_HOME%\bin\3d.exe --__arg0 everparse.cmd --clang_format_executable %QD_HOME%\bin\clang-format.exe %*
 exit /b %ERRORLEVEL%
--- src/package/everparse.sh
+++ src/package/everparse.sh
@@ -3,7 +3,7 @@ set -e
 unset CDPATH
 export QD_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export FSTAR_HOME="$QD_HOME"
-export KREMLIN_HOME="$QD_HOME"
+export KRML_HOME="$QD_HOME"
 export PATH="$QD_HOME/bin:$PATH" # because of z3
 export LD_LIBRARY_PATH="$QD_HOME/bin:$LD_LIBRARY_PATH"
 if which clang-format >/dev/null ; then
