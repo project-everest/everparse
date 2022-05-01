f2fcd977fe699632a2f9b12b1e01d41af4121a95
Tahina Ramananandro <taramana@microsoft.com>
2022-04-30 23:14:57 -0700

    avoid EVERPARSE_HOME confusion with F* CI image

--- .docker/build/build-hierarchic.sh
+++ .docker/build/build-hierarchic.sh
@@ -11,6 +11,10 @@ build_home="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "$build_home"/build_funs.sh
 . "$build_home"/install-krml-funs.sh
 
+# Clear EVERPARSE_HOME, which was set by F*'s build
+unset EVERPARSE_HOME
+sed -i -E "s|^EVERPARSE_HOME=.*||" ~/.bashrc
+
 rootPath=$(pwd)
 result_file="result.txt"
 status_file="status.txt"
