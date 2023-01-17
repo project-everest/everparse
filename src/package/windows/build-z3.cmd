10577dbd1275f1cc4f64727843bac695e4f94950
Tahina Ramananandro <taramana@microsoft.com>
2023-01-17 13:12:42 -0800

    call nmake with vsdevcmd
    from https://github.com/microsoft/vswhere/wiki/Start-Developer-Command-Prompt

--- src/package/windows/build-z3.cmd
+++ src/package/windows/build-z3.cmd
@@ -4,4 +4,9 @@ set mypath=%mypath0:~0,-1%
 cd %mypath%\z3-sources
 python scripts/mk_make.py -x
 cd build
-nmake
+for /f "usebackq delims=" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -prerelease -latest -property installationPath`) do (
+  if exist "%%i\Common7\Tools\vsdevcmd.bat" (
+    call "%%i\Common7\Tools\vsdevcmd.bat" %* && nmake
+    exit /b
+  )
+)
