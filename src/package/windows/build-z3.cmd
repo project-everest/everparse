0276b932b58d384ede542580dd369e9df7aa7aa3
Tahina Ramananandro <taramana@microsoft.com>
2023-01-17 18:09:37 -0800

    call vsdevcmd with -arch=amd64

--- src/package/windows/build-z3.cmd
+++ src/package/windows/build-z3.cmd
@@ -6,7 +6,7 @@ python scripts/mk_make.py -x
 cd build
 for /f "usebackq delims=" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -prerelease -latest -property installationPath`) do (
   if exist "%%i\Common7\Tools\vsdevcmd.bat" (
-    call "%%i\Common7\Tools\vsdevcmd.bat" %* && nmake
+    call "%%i\Common7\Tools\vsdevcmd.bat" -arch=amd64 %* && nmake
     exit /b
   )
 )
