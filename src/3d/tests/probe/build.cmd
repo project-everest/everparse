603425f67de9716851c0125a0c3bd9b5bb5a3f61
Tahina Ramananandro <taramana@microsoft.com>
2023-12-12 15:01:29 -0800

    explicitly pass --no_copy_everparse_h to 3D module tests

--- src/3d/tests/probe/build.cmd
+++ src/3d/tests/probe/build.cmd
@@ -1,7 +1,7 @@
 @echo off
 if "%1" == "" goto error
 if not exist obj mkdir obj
-call %1\everparse.cmd --odir obj --makefile nmake src\Probe.3d
+call %1\everparse.cmd --no_copy_everparse_h --odir obj --makefile nmake src\Probe.3d
 call nmake /f helper.NMakefile EVERPARSE_INPUT_DIR=src EVERPARSE_OUTPUT_DIR=obj EVERPARSE_HOME=%1 EVERPARSE_CMD=%1\everparse.cmd
 goto end
 
