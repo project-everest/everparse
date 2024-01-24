86aa3949de3e8610a5fc79fef8d1c6dd8070ed31
Tahina Ramananandro <taramana@microsoft.com>
2024-01-24 15:47:18 -0800

    hashchk/3d snap

--- src/3d/hashchk/3d/Hashing_Hash.fs
+++ src/3d/hashchk/3d/Hashing_Hash.fs
@@ -68,5 +68,78 @@ end);
 )))
 
 
+let c_comment_intro : Prims.string = "EverParse checksum hash"
+
+
+let hash_as_comment : Prims.string  ->  Prims.string = (fun ( file  :  Prims.string ) -> (
+
+let h = (hash file FStar_Pervasives_Native.None)
+in (Prims.strcat c_comment_intro (Prims.strcat ":" h))))
+
+type check_inplace_hashes_t =
+| AllHashes of c_files
+| OneHash of Prims.string
+
+
+let uu___is_AllHashes : check_inplace_hashes_t  ->  Prims.bool = (fun ( projectee  :  check_inplace_hashes_t ) -> (match (projectee) with
+| AllHashes (_0) -> begin
+true
+end
+| uu___ -> begin
+false
+end))
+
+
+let __proj__AllHashes__item___0 : check_inplace_hashes_t  ->  c_files = (fun ( projectee  :  check_inplace_hashes_t ) -> (match (projectee) with
+| AllHashes (_0) -> begin
+_0
+end))
+
+
+let uu___is_OneHash : check_inplace_hashes_t  ->  Prims.bool = (fun ( projectee  :  check_inplace_hashes_t ) -> (match (projectee) with
+| OneHash (_0) -> begin
+true
+end
+| uu___ -> begin
+false
+end))
+
+
+let __proj__OneHash__item___0 : check_inplace_hashes_t  ->  Prims.string = (fun ( projectee  :  check_inplace_hashes_t ) -> (match (projectee) with
+| OneHash (_0) -> begin
+_0
+end))
+
+
+let check_inplace_hashes : (Prims.string  ->  Prims.string  ->  Prims.bool)  ->  Prims.string  ->  check_inplace_hashes_t  ->  Prims.bool = (fun ( f  :  Prims.string  ->  Prims.string  ->  Prims.bool ) ( file_3d  :  Prims.string ) ( files_c  :  check_inplace_hashes_t ) -> (
+
+let h = (hash file_3d FStar_Pervasives_Native.None)
+in (match (files_c) with
+| OneHash (c_file) -> begin
+(f h c_file)
+end
+| AllHashes (files_c1) -> begin
+(FStar_List.for_all (f h) (FStar_List_Tot_Base.append ((files_c1.c)::(files_c1.h)::(match (files_c1.wrapper_c) with
+| FStar_Pervasives_Native.None -> begin
+[]
+end
+| FStar_Pervasives_Native.Some (w) -> begin
+(w)::[]
+end)) (FStar_List_Tot_Base.append (match (files_c1.wrapper_h) with
+| FStar_Pervasives_Native.None -> begin
+[]
+end
+| FStar_Pervasives_Native.Some (w) -> begin
+(w)::[]
+end) (match (files_c1.assertions) with
+| FStar_Pervasives_Native.None -> begin
+[]
+end
+| FStar_Pervasives_Native.Some (assertions) -> begin
+(assertions)::[]
+end))))
+end)))
+
+
 
 
