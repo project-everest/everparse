f939234c059008d0b4d8b2f0d6b581594de330c5
Tahina Ramananandro <taramana@microsoft.com>
2025-01-12 23:14:33 -0800

    print some location in case of syntax error

--- src/cddl/spec/ocaml/Main.ml
+++ src/cddl/spec/ocaml/Main.ml
@@ -1,15 +1,20 @@
 let elab_list = CDDL_Spec_AST_Driver.elab_list
 
+let print_position filename outx lexbuf =
+  let pos = lexbuf.Lexing.lex_curr_p in
+  Printf.fprintf outx "%s:%d:%d" filename
+    pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
+
+let parse_with_error filename lexbuf =
+  try Some (Parser.cddl Lexer.token lexbuf) with
+  | Parser.Error ->
+    Printf.fprintf stderr "%a: syntax error\n" (print_position filename) lexbuf;
+    None
+
 let parse filename =
   let ch = open_in filename in
   let lexbuf = Lexing.from_channel ch in
-  let res =
-    try
-      Some (Parser.cddl Lexer.token lexbuf)
-    with e ->
-      print_endline "Syntax error";
-      None
-  in
+  let res = parse_with_error filename lexbuf in
   close_in ch;
   res
 
