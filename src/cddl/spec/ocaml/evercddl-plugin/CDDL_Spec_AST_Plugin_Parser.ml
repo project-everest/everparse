372c63ca7218745468d99250cd820ee85c99d6d3
Tahina Ramananandro <taramana@microsoft.com>
2025-01-17 22:30:51 -0800

    catch all exceptions in the plugin

--- src/cddl/spec/ocaml/evercddl-plugin/CDDL_Spec_AST_Plugin_Parser.ml
+++ src/cddl/spec/ocaml/evercddl-plugin/CDDL_Spec_AST_Plugin_Parser.ml
@@ -1,3 +1,7 @@
-let parse filename = match ParseFromFile.parse CDDL_Spec_AST_Base.empty_name_env filename with
-  | None -> None
-  | Some (_, l) -> Some l
+let parse filename =
+  try
+    begin match ParseFromFile.parse CDDL_Spec_AST_Base.empty_name_env filename with
+    | None -> None
+    | Some (_, l) -> Some l
+    end
+  with _ -> None
