be14e5a2568e6fd11cf750af84045ca68ed25844
Tahina Ramananandro <taramana@microsoft.com>
2025-01-23 11:20:08 -0800

    add a release test mode with EVERPARSE_TEST_RELEASE

--- src/package/release.sh
+++ src/package/release.sh
@@ -10,10 +10,12 @@ if [[ -z "$EVERPARSE_HOME" ]] ; then
     export EVERPARSE_HOME=$PWD
 fi
 
-# Necessary for gh to authenticate to GitHub
-if [[ -z "$GH_TOKEN" ]] ; then
-    echo Missing environment variable: GH_TOKEN
-    exit 1
+if [[ -z "$EVERPARSE_TEST_RELEASE" ]] ; then
+    # Necessary for gh to authenticate to GitHub
+    if [[ -z "$GH_TOKEN" ]] ; then
+        echo Missing environment variable: GH_TOKEN
+        exit 1
+    fi
 fi
 
 if [[ -z "$OS" ]] ; then
@@ -31,7 +33,11 @@ fi
 if [[ -z "$EVERPARSE_RELEASE_REPO" ]] ; then
     EVERPARSE_RELEASE_REPO=everparse
 fi
-remote="https://${GH_TOKEN}@github.com/${EVERPARSE_RELEASE_ORG}/${EVERPARSE_RELEASE_REPO}.git"
+if [[ -n ${GH_TOKEN} ]] ; then
+    remote="https://${GH_TOKEN}@github.com/${EVERPARSE_RELEASE_ORG}/${EVERPARSE_RELEASE_REPO}.git"
+else
+    remote="https://github.com/${EVERPARSE_RELEASE_ORG}/${EVERPARSE_RELEASE_REPO}.git"
+fi
 
 branchname=$(git rev-parse --abbrev-ref HEAD)
 git diff --staged --exit-code --ignore-cr-at-eol
@@ -73,6 +79,7 @@ fi
 everparse_version=$(sed 's!\r!!g' $EVERPARSE_HOME/version.txt)
 everparse_last_version=$(git show --no-patch --format=%h $everparse_version || true)
 everparse_commit=$(git show --no-patch --format=%h)
+needs_tag=false
 if [[ $everparse_commit != $everparse_last_version ]] ; then
     if $is_windows ; then
         echo "This commit does not match the latest release. release.sh must be run on Linux first."
@@ -82,6 +89,7 @@ if [[ $everparse_commit != $everparse_last_version ]] ; then
     echo $everparse_version > $EVERPARSE_HOME/version.txt
     git add $EVERPARSE_HOME/version.txt
     git commit -m "Release $everparse_version"
+    needs_tag=true
     git tag $everparse_version
 fi
 export everparse_version
@@ -89,6 +97,15 @@ export everparse_version
 export everparse_nuget_version=${everparse_version:1}
 
 src/package/package.sh -zip -nuget
+
+if [[ -n "$EVERPARSE_TEST_RELEASE" ]] ; then
+    if $needs_tag ; then
+        echo "Release test mode, deleting the tag"
+        git tag -d $everparse_version
+    fi
+    echo "Release test succeeded!"
+    exit 0
+fi
 
 # push my commit and the tag
 git push $remote $branchname $everparse_version
