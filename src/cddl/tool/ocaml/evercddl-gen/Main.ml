43545bf038b4cff35db87deeec901f8ae287936d
Tahina Ramananandro <taramana@microsoft.com>
2025-03-26 11:29:03 -0700

    produce output to file

--- src/cddl/tool/ocaml/evercddl-gen/Main.ml
+++ src/cddl/tool/ocaml/evercddl-gen/Main.ml
@@ -10,10 +10,13 @@ let mname = ref "CDDLTest.Test"
 
 let lang = ref "C"
 
+let odir = ref "."
+
 let _ =
   let argspec = ref [
       ("--rust", Arg.Unit (fun _ -> lang := "Rust"), "Use the Rust EverCBOR library");
       ("--mname", Arg.String (fun m -> mname := m), "Set the module name");
+      ("--odir", Arg.String (fun d -> odir := d), "Set the output directory (default .)");
     ]
   in
   let usagemsg = "EverCDDL: Produces a F* file to generate formally verified parsers and serializers from CDDL specifications.\nUsage: "^Sys.argv.(0) ^" [options] file1 [file2 ...]" in
@@ -32,4 +35,14 @@ let _ =
      let str = CDDL_Tool_Gen.produce_defs_fst !mname !lang filenames_str l in
      if String.starts_with ~prefix:"Error: " str
      then failwith str
-     else print_endline str
+     else begin
+         let filename = !mname ^ ".fst" in
+         let filename_tmp = filename ^ ".tmp" in
+         let dir = !odir in
+         if not (Sys.file_exists dir && Sys.is_directory dir) then Sys.mkdir dir 0o755;
+         let ch = open_out (Filename.concat dir filename_tmp) in
+         output_string ch str;
+         close_out ch;
+         Sys.rename filename_tmp filename;
+         print_endline ("Success, output to: " ^ filename)
+       end
