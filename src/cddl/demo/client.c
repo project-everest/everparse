bc89cffb0338b3c0f99c75567638bdad6011cd58
Tahina Ramananandro <taramana@microsoft.com>
2025-04-02 10:07:26 +0000

    restore demo with bijections

--- src/cddl/demo/client.c
+++ src/cddl/demo/client.c
@@ -1,13 +1,13 @@
 #include "CDDLExtractionTest.h"
 
 int main(void) {
-  FStar_Pervasives_Native_option__uint64_t test_snd = {
+  FStar_Pervasives_Native_option__CDDLTest_Test_evercddl_uint_pretty test_snd = {
     .tag = FStar_Pervasives_Native_Some,
     .v = 42L
   };
-  CDDLTest_Test_evercddl_test1 test = {
-    .fst = 18L,
-    .snd = test_snd
+  CDDLTest_Test_evercddl_test1_pretty test = {
+    .x0 = 18L,
+    .x1 = test_snd
   };
   uint8_t out[32];
   Pulse_Lib_Slice_slice__uint8_t out_s = {
@@ -25,8 +25,8 @@ int main(void) {
   cbor_det_t obj = cbor_det_parse(out, sz);
   if (! (CDDLTest_Test_validate_test1(obj)))
     return 3;
-  CDDLTest_Test_evercddl_test1 ret = CDDLTest_Test_parse_test1(obj);
-  if (! (ret.fst == test.fst && ret.snd.tag == test.snd.tag && ret.snd.v == test.snd.v))
+  CDDLTest_Test_evercddl_test1_pretty ret = CDDLTest_Test_parse_test1(obj);
+  if (! (ret.x0 == test.x0 && ret.x1.tag == test.x1.tag && ret.x1.v == test.x1.v))
     return 4;
   return CDDLTest_Client_main();
 }
