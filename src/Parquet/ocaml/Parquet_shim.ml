c4e154ce9141fdc259769ba912e9954135104ecf
Yi Cai <caiyeee@outlook.com>
2025-07-24 13:09:45 -0700

    Pretty printer for `file_meta_data`

--- src/Parquet/ocaml/Parquet_shim.ml
+++ src/Parquet/ocaml/Parquet_shim.ml
@@ -125,3 +125,148 @@ let convert_file_meta_data (fm : fileMetaData) : file_meta_data =
     encryption_algorithm        = None ;
     footer_signing_key_metadata = fm#get_footer_signing_key_metadata ;
   }
+
+
+(*----------------------------------------------------------------------
+   pretty printer                                                            
+  --------------------------------------------------------------------*)
+open Format
+(* generic helpers --------------------------------------------------- *)
+let pp_string fmt s = fprintf fmt "%S" s
+
+(* let list_sep fmt () = pp_print_string fmt "; "      (* “; ” with break *) *)
+let list_sep = pp_print_cut
+let pp_list pp_elt fmt lst =
+  fprintf fmt "[@[%a@]]" (pp_print_list ~pp_sep:list_sep pp_elt) lst
+
+let pp_opt pp fmt = function
+  | None   -> pp_print_string fmt "None"
+  | Some x -> fprintf fmt "Some(@[<v>%a@])" pp x
+let pp_i32 fmt i = pp_print_string fmt (FStar_Int32.to_string i)
+
+let pp_i64 fmt i = pp_print_string fmt (FStar_Int64.to_string i)
+let pp_i16 fmt i = pp_print_string fmt (FStar_Int16.to_string i)
+let pp_i8  fmt i = pp_print_string fmt (FStar_Int8.to_string  i)
+
+(* enum printers ----------------------------------------------------- *)
+let pp_physical_type fmt = function
+  | BOOLEAN            -> pp_print_string fmt "BOOLEAN"
+  | INT32              -> pp_print_string fmt "INT32"
+  | INT64              -> pp_print_string fmt "INT64"
+  | INT96              -> pp_print_string fmt "INT96"
+  | FLOAT              -> pp_print_string fmt "FLOAT"
+  | DOUBLE             -> pp_print_string fmt "DOUBLE"
+  | BYTE_ARRAY         -> pp_print_string fmt "BYTE_ARRAY"
+  | FIXED_LEN_BYTE_ARRAY -> pp_print_string fmt "FIXED_LEN_BYTE_ARRAY"
+
+let pp_repetition fmt = function
+  | REQUIRED -> pp_print_string fmt "REQUIRED"
+  | OPTIONAL -> pp_print_string fmt "OPTIONAL"
+  | REPEATED -> pp_print_string fmt "REPEATED"
+
+let pp_encoding fmt = function
+  | PLAIN                   -> pp_print_string fmt "PLAIN"
+  | PLAIN_DICTIONARY        -> pp_print_string fmt "PLAIN_DICTIONARY"
+  | RLE                     -> pp_print_string fmt "RLE"
+  | BIT_PACKED              -> pp_print_string fmt "BIT_PACKED"
+  | DELTA_BINARY_PACKED     -> pp_print_string fmt "DELTA_BINARY_PACKED"
+  | DELTA_LENGTH_BYTE_ARRAY -> pp_print_string fmt "DELTA_LENGTH_BYTE_ARRAY"
+  | DELTA_BYTE_ARRAY        -> pp_print_string fmt "DELTA_BYTE_ARRAY"
+  | RLE_DICTIONARY          -> pp_print_string fmt "RLE_DICTIONARY"
+  | BYTE_STREAM_SPLIT       -> pp_print_string fmt "BYTE_STREAM_SPLIT"
+
+let pp_codec fmt = function
+  | UNCOMPRESSED -> pp_print_string fmt "UNCOMPRESSED"
+  | SNAPPY       -> pp_print_string fmt "SNAPPY"
+  | GZIP         -> pp_print_string fmt "GZIP"
+  | LZO          -> pp_print_string fmt "LZO"
+  | BROTLI       -> pp_print_string fmt "BROTLI"
+  | LZ4          -> pp_print_string fmt "LZ4"
+  | ZSTD         -> pp_print_string fmt "ZSTD"
+  | LZ4_RAW      -> pp_print_string fmt "LZ4_RAW"
+
+(* schema_element ---------------------------------------------------- *)
+let pp_schema_element fmt se =
+  fprintf fmt "@[<v2>{@,\
+    @[<hov2>name = \"%s\";@]@,\
+    @[<hov2>rep = %a;@]@,\
+    @[<hov2>type = %a;@]@,\
+    @[<hov2>num_children = %a@]@,\
+  }@]"
+    se.name
+    (pp_opt pp_repetition) se.repetition_type
+    (pp_opt pp_physical_type) se.type_
+    (pp_opt pp_i32) se.num_children
+
+let pp_path fmt p = pp_string fmt (String.concat "." p)
+
+(* column_meta_data (trimmed to fields used by validator) ------------ *)
+let pp_column_meta fmt cm =
+  fprintf fmt
+    "@[<v 2>{@,\
+    path                 = %a;@,\
+    codec                = %a;@,\
+    values               = %a;@,\
+    comp_size            = %a;@,\
+    data_page_offset     = %a;@,\
+    index_page_offset    = %a;@,\
+    dictionary_page_offset = %a;@,\
+    }@]"
+    pp_path cm.path_in_schema
+    pp_codec cm.codec
+    pp_i64  cm.num_values3
+    pp_i64  cm.total_compressed_size
+    pp_i64  cm.data_page_offset
+    (pp_opt pp_i64) cm.index_page_offset
+    (pp_opt pp_i64) cm.dictionary_page_offset
+
+(* column_chunk ------------------------------------------------------ *)
+let pp_column_chunk fmt cc =
+  fprintf fmt "@[<v 2>{@,\
+  file_off = %a;@,\
+  meta = %a;@,\
+  offset_index_offset = %a;@,\
+  offset_index_length = %a;@,\
+  column_index_offset = %a;@,\
+  column_index_length = %a@,\
+}@]"
+    pp_i64 cc.file_offset
+    (pp_opt pp_column_meta) cc.meta_data
+    (pp_opt pp_i64) cc.offset_index_offset
+    (pp_opt pp_i32) cc.offset_index_length
+    (pp_opt pp_i64) cc.column_index_offset
+    (pp_opt pp_i32) cc.column_index_length
+
+(* row_group --------------------------------------------------------- *)
+let pp_row_group fmt rg =
+  fprintf fmt "@[<v 2>{@,\
+  rows                = %a;@,\
+  total_sz            = %a;@,\
+  columns             = %a;@,\
+  file_offset         = %a;@,\
+  total_compressed_sz = %a;@,\
+  ordinal             = %a@,\
+}@]"
+    pp_i64 rg.num_rows1
+    pp_i64 rg.total_byte_size
+    (pp_list pp_column_chunk) rg.columns
+    (pp_opt pp_i64) rg.file_offset1
+    (pp_opt pp_i64) rg.total_compressed_size1
+    (pp_opt pp_i16) rg.ordinal
+
+(* top-level --------------------------------------------------------- *)
+let pp_file_meta_data fmt fmd =
+  fprintf fmt "@[<v 2>{@,\
+  version   = %a;@,\
+  num_rows  = %a;@,\
+  schema    = %a;@,\
+  row_groups= %a@,\
+  @]}@]"
+    pp_i32   fmd.version
+    pp_i64   fmd.num_rows2
+    (pp_list pp_schema_element) fmd.schema
+    (pp_list pp_row_group)      fmd.row_groups
+
+(* convenience string renderers ------------------------------------- *)
+let show pp v = Format.asprintf "%a" pp v
+let show_file_meta_data = show pp_file_meta_data
