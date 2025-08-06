b018e48c9eb96c89e7a170095c38076ec341adc3
Tahina Ramananandro <taramana@microsoft.com>
2025-08-06 23:40:20 +0000

    enable build-evercddl on MacOS, add a CI test

--- build-evercddl.sh
+++ build-evercddl.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 set -x
-make _opam
-opam exec -- make -j -C opt
-opam exec -- make -j 8 -k cddl
+unset CDPATH
+MAKE=$(which gmake >/dev/null 2>&1 && echo gmake || echo make)
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+"$MAKE" _opam
+opam exec -- "$MAKE" -j -C opt
+opam exec -- "$MAKE" -j 8 -k cddl
