4ce00c0fbbfc705398656fe1c9206a89f053ee1a
Yi Cai <caiyeee@outlook.com>
2025-08-22 15:15:24 -0700

    Signatures for `validate_footer` and `read_footer`

--- src/Parquet/cpp/shim_parquet_pulse.hpp
+++ src/Parquet/cpp/shim_parquet_pulse.hpp
@@ -11,6 +11,12 @@ namespace shim_parquet_pulse {
 Parquet_Pulse_Toplevel_file_meta_data to_pulse_file_metadata(
     const parquet::FileMetaData& src);
 
+// input: &file[0..file.len() - 8]
+// poffset: [0, input.len], or more specifically, it will be file.len() - 8 - footer_len
+bool validate_footer(Parquet_Pulse_Toplevel_bytes input, size_t* poffset);
+
+Parquet_Pulse_Toplevel_file_meta_data read_footer(Parquet_Pulse_Toplevel_bytes input);
+
 void free_file_metadata(Parquet_Pulse_Toplevel_file_meta_data& m);
 
 }  // namespace shim_parquet_pulse
