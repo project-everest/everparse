15d875b0924af4ec2037a1306b52ababea200253
Tahina Ramananandro <taramana@microsoft.com>
2025-08-23 20:18:51 +0000

    opam-env.sh: make PATH update compositional

--- opt/opam-env.sh
+++ opt/opam-env.sh
@@ -2,4 +2,5 @@
 unset CDPATH
 SED=$(which gsed >/dev/null 2>&1 && echo gsed || echo sed)
 # Print OPAM environment in GNU Makefile format
-opam env --root=opam --set-root --shell=sh | $SED 's!^\([^=]*\)='"'"'\(.*\)'"'"'; export [^;]*;$!export \1 := \2!'
+opam env --root=opam --set-root --shell=sh | grep -v '^PATH=' | $SED 's!^\([^=]*\)='"'"'\(.*\)'"'"'; export [^;]*;$!export \1 := \2!'
+echo 'export PATH := '"$(pwd)/opam/default/bin"':$(PATH)'
