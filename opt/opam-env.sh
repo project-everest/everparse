28c6bf7a83df0e9749560dc9042db253daf6bbab
Guido Martínez <mtzguido@gmail.com>
2025-08-28 17:35:52 -0700

    Some fixes to prevent using external OCamls

--- opt/opam-env.sh
+++ opt/opam-env.sh
@@ -3,4 +3,4 @@ unset CDPATH
 SED=$(which gsed >/dev/null 2>&1 && echo gsed || echo sed)
 # Print OPAM environment in GNU Makefile format
 opam env --root=opam --set-root --shell=sh | grep -v '^PATH=' | $SED 's!^\([^=]*\)='"'"'\(.*\)'"'"'; export [^;]*;$!export \1 := \2!'
-echo 'export PATH := '"$(pwd)/opam/default/bin"':$(PATH)'
+echo 'export PATH := '"$(pwd)/opam/$(opam switch --root=opam show)/bin"':$(PATH)'
