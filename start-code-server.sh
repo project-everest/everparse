b2c128c8c5e449b4ed63da9215c7222f89e5422e
Tahina Ramananandro <taramana@microsoft.com>
2025-08-30 21:53:30 +0000

    fix the VSCode configuration

--- start-code-server.sh
+++ start-code-server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+unset CDPATH
+cd "$( dirname "${BASH_SOURCE[0]}" )"
 nohup code-server --auth none --bind-addr 0.0.0.0:8080 --disable-workspace-trust . \
     >>code-server.log 2>&1 &
 disown
