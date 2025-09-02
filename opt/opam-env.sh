4c8a9f503d579575021a211a58357e79db201d38
Tahina Ramananandro <taramana@microsoft.com>
2025-09-02 15:25:08 -0700

    fix opam environment

--- opt/opam-env.sh
+++ opt/opam-env.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 unset CDPATH
+cd "$( dirname "${BASH_SOURCE[0]}" )"
 SED=$(which gsed >/dev/null 2>&1 && echo gsed || echo sed)
 if [[ -z "$EVERPARSE_USE_OPAMROOT" ]] ; then
-    OPAMROOT=$(pwd)/opam
-    root_opam=--root=opam
-else
-    if [[ -z "$OPAMROOT" ]] ; then
+        OPAMROOT="$(pwd)/opam"
+elif [[ -z "$OPAMROOT" ]] ; then
 	OPAMROOT="$(opam var root)"
-    fi
-    root_opam="--root=$OPAMROOT"
 fi
+root_opam="--root=$OPAMROOT"
 opam env "$root_opam" --set-root --shell=sh | grep -v '^PATH=' |
     if [[ "$1" = --shell ]] ; then
 	cat
