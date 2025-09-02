039b5284d1f3b86794c9ef3233f2e60cb5bb5a71
Tahina Ramananandro <taramana@microsoft.com>
2025-09-02 15:24:41 +0000

    do not download opam packages if OPAMROOT is set

--- opt/opam-env.sh
+++ opt/opam-env.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 unset CDPATH
 SED=$(which gsed >/dev/null 2>&1 && echo gsed || echo sed)
-# Print OPAM environment in GNU Makefile format
-opam env --root=opam --set-root --shell=sh | grep -v '^PATH=' | $SED 's!^\([^=]*\)='"'"'\(.*\)'"'"'; export [^;]*;$!export \1 := \2!'
-echo 'export PATH := '"$(pwd)/opam/$(opam switch --root=opam show)/bin"':$(PATH)'
+if [[ -z "$OPAMROOT" ]] ; then
+    OPAMROOT=$(pwd)/opam
+    root_opam=--root=opam
+else
+    root_opam="--root=$OPAMROOT"
+fi
+opam env "$root_opam" --set-root --shell=sh | grep -v '^PATH=' |
+    if [[ "$1" = --shell ]] ; then
+	cat
+    else
+	$SED 's!^\([^=]*\)='"'"'\(.*\)'"'"'; export [^;]*;$!export \1 := \2!'
+    fi
+if [[ "$1" = --shell ]] ; then
+    equal="='"
+    epath="'"':"$PATH"'
+else
+    equal=':='
+    epath=':$(PATH)'
+fi
+echo 'export PATH'$equal"$OPAMROOT/$(opam switch "$root_opam" show)/bin$epath"
