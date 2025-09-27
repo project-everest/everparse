1e999d01d75ee8a31e70d0ca62a25d0567c71c72
Tahina Ramananandro <taramana@microsoft.com>
2025-09-27 00:02:15 +0000

    Revert "Merge branch 'release-2025.09.17' into _release"
    This reverts commit 12b0f133bcb1d9bee0407edf262f3ee67000563e, reversing
    changes made to ca9a3ea15032e3f6b16c54ef9308ffc56e69764c.

--- fstar-src/fstar/dune/fstar-plugins/plugins.ml/FStar_Classical.ml
+++
@@ -1,8 +0,0 @@
-open Fstarcompiler
-open Prims
-let get_squashed : 'b 'a . unit -> 'a =
-  fun uu___ -> (fun uu___ -> Obj.magic ()) uu___
-
-
-
-
--- fstar-src/fstar/dune/libplugin/ulib.pluginml/FStar_Sealed_Inhabited.ml
+++
@@ -1,7 +0,0 @@
-open Fstarcompiler
-open Prims
-type ('a, 'witness) sealed_ = 'a FStar_Sealed.sealed
-type ('a, 'witness, 'x) is_sealed = unit
-type ('a, 'witness) sealed = ('a, 'witness) sealed_
-let seal : 'a . 'a -> 'a -> ('a, Obj.t) sealed =
-  fun w -> fun x -> FStar_Sealed.seal x
