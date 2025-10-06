6861e56d9c01bae7d3c5dc7c07a8e9d21802fbfd
Dzomo, the Everest Yak <24394600+dzomo@users.noreply.github.com>
2025-10-06 22:59:40 +0000

    Revert "Merge commit '9455c997bb5c140c317026cd603dee3ef05568c9' into _release"
    This reverts commit dd91557136ebf75ba9805fe9b5a20fa28f2d76dd, reversing
    changes made to c7644590b0032dfbff38855f4decf5d8a6abcde0.

--- opt/FStar/dune/libplugin/ulib.pluginml/LowStar_Comment.ml
+++
@@ -1,5 +0,0 @@
-open Fstarcompiler
-open Prims
-let comment_gen : 't . Prims.string -> 't -> Prims.string -> 't =
-  fun before -> fun body -> fun after -> body
-let (comment : Prims.string -> unit) = fun s -> ()
--- opt/FStar/src/class/FStarC.Class.HasRange.fst
+++
@@ -1,8 +0,0 @@
-module FStarC.Class.HasRange
-
-open FStarC.Range
-
-instance hasRange_range : hasRange range = {
-  pos = id;
-  setPos = (fun r _ -> r); // not really used
-}
