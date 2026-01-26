4fac1524be1236d86b49f02b1cfbc6e46cc86c5d
Tahina Ramananandro <taramana@microsoft.com>
2026-01-26 22:22:50 +0000

    rewrite the CDDL syntax parser to determine typ vs. group names after the fact
    from RFC 8610 Appendix C:
    
       Note that whether the name
       defined by a rule stands for a type or a group isn't always
       determined by syntax alone: e.g., "a = b" can make "a" a type if "b"
       is a type, or a group if "b" is a group.  More subtly, in "a = (b)",
       "a" may be used as a type if "b" is a type, or as a group both when
       "b" is a group and when "b" is a type (a good convention to make the
       latter case stand out to the human reader is to write "a = (b,)").
       (Note that the same dual meaning of parentheses applies within an
       expression but often can be resolved by the context of the
       parenthesized expression.  On the more general point, it may not be
       clear immediately either whether "b" stands for a group or a type --
       this semantic processing may need to span several levels of rule
       definitions before a determination can be made.)

--- src/cddl/tool/ocaml/evercddl-lib/CDDLParser.ml
+++ src/cddl/tool/ocaml/evercddl-lib/CDDLParser.ml
@@ -4,10 +4,18 @@ open Fstar_pluginlib
 open FStar_Pervasives
 open CDDL_Spec_AST_Base
 
+type id_kind =
+| Regular
+| SocketType
+| SocketGroup
+
+type 'a thing_or_name = | Thing of 'a | Name of string
+
+type program0 = (Prims.string * decl thing_or_name) Prims.list
+
 type state = {
-  env: CDDLNameEnv.name_env;
-  sockets: string list;
-  result: CDDL_Spec_AST_Base.program;
+  sockets: (string * id_kind) list;
+  result: program0;
 }
 type cddl_t = state
 type 'a parser = (Tokens.token, state, 'a, cddl_t) ABNF.parser
@@ -42,11 +50,6 @@ let debug
                   )
          )
       )
-
-type id_kind =
-| Regular
-| SocketType
-| SocketGroup
 
 let terminal name f =
   terminal (fun x ->
@@ -113,82 +116,60 @@ let id = debug "id"
     concat dollardollar (fun _ -> concat raw_id (fun s -> ret (SocketGroup, s)));
   ])
 
-let typename = debug "typename"
+let name =
   (concat id (fun ((k, n) as res) -> concat (get_state ()) (fun s ->
-    match k with
-    | Regular ->
-      if List.mem n s.sockets
-      then fail
-      else begin match CDDLNameEnv.check_name s.env n CDDL_Spec_AST_Base.NType with
-      | None -> fail
-      | Some s' ->
-        let s' = { s with env = s' } in
-        concat (set_state s') (fun _ -> ret res)
-      end
-    | SocketType ->
-      if not (List.mem n s.sockets)
-      then begin match CDDLNameEnv.cddl_env_of_env s.env n with
-      | None ->
-        let s' = { s with
-          env = CDDLNameEnv.extend_name_env s.env n CDDL_Spec_AST_Base.NType;
-          sockets = n :: s.sockets;
-        }
-        in
-        concat (set_state s') (fun _ -> ret res)
-      | _ -> fail
-      end
-      else begin match CDDLNameEnv.cddl_env_of_env s.env n with
-      | Some CDDL_Spec_AST_Base.NType -> ret res
-      | _ -> fail
-      end
-    | _ -> fail
+                                         begin match List.assoc_opt n s.sockets with
+                                         | None ->
+                                            let s' = { s with sockets = List.merge (fun (_, n1) (_, n2) -> compare n1 n2) [(n, k)] s.sockets } in
+                                            concat (set_state s') (fun _ -> ret res)
+                                         | Some k' ->
+                                            if k = k'
+                                            then ret res
+                                            else fail
+                                         end
   )))
 
+let typename = debug "typename"
+  (concat name (fun ((k, _) as res) -> match k with SocketGroup -> fail | _ -> ret res))
+
 let groupname = debug "groupname"
-  (concat id (fun ((k, n) as res) -> concat (get_state ()) (fun s ->
-    match k with
-    | Regular ->
-      if List.mem n s.sockets
-      then fail
-      else begin match CDDLNameEnv.check_name s.env n CDDL_Spec_AST_Base.NGroup with
-      | None -> fail
-      | Some s' ->
-        let s' = { s with env = s' } in
-        concat (set_state s') (fun _ -> ret res)
-      end
-    | SocketGroup ->
-      if not (List.mem n s.sockets)
-      then begin match CDDLNameEnv.cddl_env_of_env s.env n with
-      | None ->
-        let s' = { s with
-          env = CDDLNameEnv.extend_name_env s.env n CDDL_Spec_AST_Base.NGroup;
-          sockets = n :: s.sockets;
-        }
-        in
-        concat (set_state s') (fun _ -> ret res)
-      | _ -> fail
-      end
-      else begin match CDDLNameEnv.cddl_env_of_env s.env n with
-      | Some CDDL_Spec_AST_Base.NGroup -> ret res
-      | _ -> fail
-      end
-    | _ -> fail
-  )))
+  (concat name (fun ((k, _) as res) -> match k with SocketType -> fail | _ -> ret res))
+
+let close_typ (t: typ thing_or_name) : typ =
+  match t with
+  | Thing t' -> t'
+  | Name n -> TDef n
+
+let close_group (t: group thing_or_name) : group =
+  match t with
+  | Thing t' -> t'
+  | Name n -> GDef n
 
 let assignt ((k, x): (id_kind * string)) = debug "assignt"
   (choice
     (concat eq (fun _ ->
       if k = Regular
-      then ret (fun (t: typ) (l: (string * CDDL_Spec_AST_Base.decl) list) -> (x, CDDL_Spec_AST_Base.DType t) :: l)
+      then ret (fun (t: typ thing_or_name) (l: (string * CDDL_Spec_AST_Base.decl thing_or_name) list) ->
+               let t' = match t with
+               | Thing t' -> Thing (CDDL_Spec_AST_Base.DType t')
+               | Name n -> Name n
+               in
+               (x, t') :: l)
       else fail
     ))
     (concat slasheq (fun _ ->
       if k = SocketType
-      then ret (fun (t: typ) (l: (string * CDDL_Spec_AST_Base.decl) list) ->
+      then ret (fun (t: typ thing_or_name) (l: (string * CDDL_Spec_AST_Base.decl thing_or_name) list) ->
+        let t = close_typ t in
         match List.assoc_opt x l with
-        | None -> (x, CDDL_Spec_AST_Base.DType t) :: l
-        | Some (CDDL_Spec_AST_Base.DType t0) -> (x, CDDL_Spec_AST_Base.DType (CDDL_Spec_AST_Elab_Base.mk_TChoice t0 t)) :: List.remove_assoc x l
-        | _ -> failwith "assignt: this should not happen. Please report"
+        | None -> (x, Thing (CDDL_Spec_AST_Base.DType t)) :: l
+        | Some t_ ->
+           let t0 = match t_ with
+             | Thing (CDDL_Spec_AST_Base.DType t0) -> t0
+             | Name n -> TDef n
+             | _ -> failwith "assignt: this should not happen. Please report"
+           in
+           (x, Thing (CDDL_Spec_AST_Base.DType (CDDL_Spec_AST_Elab_Base.mk_TChoice t0 t))) :: List.remove_assoc x l
       )
       else fail
     ))
@@ -198,16 +179,27 @@ let assigng ((k, x) : (id_kind * string)) = debug "assigng"
   (choice
     (concat eq (fun _ ->
       if k = Regular
-      then ret (fun (t: group) (l: (string * CDDL_Spec_AST_Base.decl) list) -> (x, CDDL_Spec_AST_Base.DGroup t) :: l)
+      then ret (fun (t: group thing_or_name) (l: (string * CDDL_Spec_AST_Base.decl thing_or_name) list) ->
+               let t' = match t with
+                 | Thing t' -> Thing (CDDL_Spec_AST_Base.DGroup t')
+                 | Name n -> Name n
+               in
+               (x, t') :: l)
       else fail
     ))
     (concat slashslasheq (fun _ ->
       if k = SocketGroup
-      then ret (fun (t: group) (l: (string * CDDL_Spec_AST_Base.decl) list) ->
+      then ret (fun (t: group thing_or_name) (l: (string * CDDL_Spec_AST_Base.decl thing_or_name) list) ->
+        let t = close_group t in
         match List.assoc_opt x l with
-        | None -> (x, CDDL_Spec_AST_Base.DGroup t) :: l
-        | Some (CDDL_Spec_AST_Base.DGroup t0) -> (x, CDDL_Spec_AST_Base.DGroup (CDDL_Spec_AST_Driver.mk_GChoice t0 t)) :: List.remove_assoc x l
-        | _ -> failwith "assigng: this should not happen. Please report"
+        | None -> (x, Thing (CDDL_Spec_AST_Base.DGroup t)) :: l
+        | Some t_ ->
+           let t0 = match t_ with
+             | Thing (CDDL_Spec_AST_Base.DGroup t0) -> t0
+             | Name n -> GDef n
+             | _ -> failwith "assigng: this should not happen. Please report"
+           in
+           (x, Thing (CDDL_Spec_AST_Base.DGroup (CDDL_Spec_AST_Driver.mk_GChoice t0 t))) :: List.remove_assoc x l
       )
       else fail
     ))
@@ -257,17 +249,17 @@ let occur_bound = debug "occur_from" (
 let occur = debug "occur" (
   choices
     [
-      concat plus (fun _ -> ret (fun x -> GOneOrMore x));
-      concat question (fun _ -> ret (fun x -> GZeroOrOne x));
+      concat plus (fun _ -> ret (fun x -> Thing (GOneOrMore (close_group x))));
+      concat question (fun _ -> ret (fun x -> Thing (GZeroOrOne (close_group x))));
       concat occur_bound (fun from -> concat star (fun _ -> concat occur_bound (fun to_ -> (* TODO: bounds *)
-       if from = None && to_ = None then ret (fun x -> GZeroOrMore x) else fail)));
+       if from = None && to_ = None then ret (fun x -> Thing (GZeroOrMore (close_group x))) else fail)));
     ]
 )
 
 let option_occur = debug "option_occur" (
   choice
     (concat occur (fun x -> concat s (fun _ -> ret x)))
-    (ret (fun (x: group) -> x))
+    (ret (fun (x: group thing_or_name) -> x))
 )
 
 let optcom = debug "optcom" (concat s (fun _ -> option (concat comma (fun _ -> s))))
@@ -275,15 +267,15 @@ let optcom = debug "optcom" (concat s (fun _ -> option (concat comma (fun _ -> s
 let rangeop =
   debug "rangeop"
     (choice
-      (concat dotdotdot (fun _ -> ret (fun t1 t2 -> TRange (t1, false, t2))))
-      (concat dotdot (fun _ -> ret (fun t1 t2 -> TRange (t1, true, t2))))
+      (concat dotdotdot (fun _ -> ret (fun t1 t2 -> Thing (TRange (close_typ t1, false, close_typ t2)))))
+      (concat dotdot (fun _ -> ret (fun t1 t2 -> Thing (TRange (close_typ t1, true, close_typ t2)))))
     )
 
 let ctlop =
   debug "ctlop"
     (concat dot (fun _ -> concat id (fun s -> match s with
-    | (Regular, "size") -> ret (fun t1 t2 -> TSize (t1, t2))
-    | (Regular, "everparse-det-cbor") -> ret (fun t1 t2 -> TDetCbor (t1, t2))
+    | (Regular, "size") -> ret (fun t1 t2 -> Thing (TSize (close_typ t1, close_typ t2)))
+    | (Regular, "everparse-det-cbor") -> ret (fun t1 t2 -> Thing (TDetCbor (close_typ t1, close_typ t2)))
     (* TODO: (non-injective) cbor *)
     | _ -> fail
     )))
@@ -295,14 +287,18 @@ let rangeop_or_ctlop =
       (concat ctlop (fun f -> ret f))
     )
 
+let close_group_thing_option x = match x with
+| None -> Thing GNop
+| Some x -> x
+
 let rec type_ () = debug "type" (
   concat (type1 ()) (fun x -> concat (type_tail ()) (fun xs -> ret (xs x)))
 )
 
 and type_tail () = debug "type_tail" (
   choice
-    (concat s (fun _ -> concat slash (fun _ -> concat s (fun _ -> concat (type1 ()) (fun xl -> concat (type_tail ()) (fun xr -> ret (fun (x: typ) -> CDDL_Spec_AST_Elab_Base.mk_TChoice x (xr xl))))))))
-    (ret (fun (x: typ) -> x))
+    (concat s (fun _ -> concat slash (fun _ -> concat s (fun _ -> concat (type1 ()) (fun xl -> concat (type_tail ()) (fun (xr: typ thing_or_name -> typ thing_or_name) -> ret (fun (x: typ thing_or_name) -> Thing (CDDL_Spec_AST_Elab_Base.mk_TChoice (close_typ x) (close_typ (xr xl))))))))))
+    (ret (fun (x: typ thing_or_name) -> x))
 )
 
 and type1 () = debug "type1" (concat (type2 ()) (fun (t, needs_space) -> concat (type1_tail needs_space) (fun f -> ret (f t))))
@@ -318,47 +314,64 @@ and type1_tail (needs_space: bool) =
 and type2 () = debug "type2" (
   choices
     [
-      concat value (fun x -> ret (TElem (ELiteral x), false));
-      concat typename (fun (_, x) -> (* option(genericarg) *) ret (TDef x, true));
+      concat value (fun x -> ret (Thing (TElem (ELiteral x)), false));
+      concat typename (fun (k, x) -> (* option(genericarg) *) ret (
+                                         begin match k with
+                                         | Regular -> Name x
+                                         | SocketType -> Thing (TDef x)
+                                         | _ -> failwith "type2: typename"
+                                         end, true));
       concat lparen (fun _ -> concat s (fun _ -> concat (type_ ()) (fun x -> concat s (fun _ -> concat rparen (fun _ -> ret (x, false))))));
-      concat lbrace (fun _ -> concat s (fun _ -> concat (group ()) (fun x -> concat s (fun _ -> concat rbrace (fun _ -> ret (TMap x, false))))));
-      concat lbrack (fun _ -> concat s (fun _ -> concat (group ()) (fun x -> concat s (fun _ -> concat rbrack (fun _ -> ret (TArray x, false))))));
+      concat lbrace (fun _ -> concat s (fun _ -> concat (group ()) (fun x -> concat s (fun _ -> concat rbrace (fun _ -> ret (Thing (TMap (close_group x)), false))))));
+      concat lbrack (fun _ -> concat s (fun _ -> concat (group ()) (fun x -> concat s (fun _ -> concat rbrack (fun _ -> ret (Thing (TArray (close_group x)), false))))));
 (* TODO: "~" s typename option(genericarg) *)
 (* TODO: "&" s "(" s group s ")" *)
-      concat amp (fun _ -> concat s (fun _ -> concat lparen (fun _ -> concat s (fun _ -> concat bareword (fun (_, name) -> concat s (fun _ -> concat colon (fun _ -> concat s (fun _ -> concat (type_ ()) (fun ty -> concat s (fun _ -> concat rparen (fun _ -> ret (TNamed (name, ty), false))))))))))));
+      concat amp (fun _ -> concat s (fun _ -> concat lparen (fun _ -> concat s (fun _ -> concat bareword (fun (_, name) -> concat s (fun _ -> concat colon (fun _ -> concat s (fun _ -> concat (type_ ()) (fun ty -> concat s (fun _ -> concat rparen (fun _ -> ret (Thing (TNamed (name, close_typ ty)), false))))))))))));
 (* TODO: "&" s groupname option(genericarg) *)
-      concat pound6 (fun _ -> concat (option tag) (fun tag -> concat lparen (fun _ -> concat s (fun _ -> concat (type_ ()) (fun x -> concat s (fun _ -> concat rparen (fun _ -> ret (TTagged (tag, x), false))))))));
+      concat pound6 (fun _ -> concat (option tag) (fun tag -> concat lparen (fun _ -> concat s (fun _ -> concat (type_ ()) (fun x -> concat s (fun _ -> concat rparen (fun _ -> ret (Thing (TTagged (tag, close_typ x)), false))))))));
 (* TODO: generalize "#"DIGIT option(tag) *)
-      concat pound0 (fun _ -> ret (TElem EUInt, false));
-      concat pound1 (fun _ -> ret (TElem ENInt, false));
-      concat pound2 (fun _ -> ret (TElem EByteString, false));
-      concat pound3 (fun _ -> ret (TElem ETextString, false));
-      concat pound7 (fun _ -> concat (option tag) (fun tag -> ret ((match tag with None -> TElem ESimple | Some v -> TElem (ELiteral (LSimple v))), false)));
-      concat pound (fun _ -> ret (TElem EAny, false));
+      concat pound0 (fun _ -> ret (Thing (TElem EUInt), false));
+      concat pound1 (fun _ -> ret (Thing (TElem ENInt), false));
+      concat pound2 (fun _ -> ret (Thing (TElem EByteString), false));
+      concat pound3 (fun _ -> ret (Thing (TElem ETextString), false));
+      concat pound7 (fun _ -> concat (option tag) (fun tag -> ret (Thing (match tag with None -> TElem ESimple | Some v -> TElem (ELiteral (LSimple v))), false)));
+      concat pound (fun _ -> ret (Thing (TElem EAny), false));
     ]
 )
 
-and group () = debug "group" (
+and group () : group thing_or_name parser = debug "group" (
   concat (grpchoice ()) (fun a -> concat (group_tail ()) (fun q -> ret (q a)))
 )
 
-and group_tail () = debug "group_tail" (
+and group_tail () : (group thing_or_name option -> group thing_or_name) parser = debug "group_tail" (
   choice
-    (concat s (fun _ -> concat slashslash (fun _ -> concat s (fun _ -> concat (grpchoice ()) (fun a -> concat (group_tail ()) (fun q -> ret (fun (x: group) -> CDDL_Spec_AST_Driver.mk_GChoice x (q a))))))))
-    (ret (fun (x: group) -> x))
+    (concat s (fun _ -> concat slashslash (fun _ -> concat s (fun _ -> concat (grpchoice ()) (fun a -> concat (group_tail ()) (fun q -> ret (fun (x: group thing_or_name option) -> Thing (CDDL_Spec_AST_Driver.mk_GChoice (close_group (close_group_thing_option x)) (close_group (q a))))))))))
+    (ret (fun (x: group thing_or_name option) -> close_group_thing_option x))
 )
 
 and grpchoice () = debug "grpchoice" (
   choice
-    (concat (grpent ()) (fun a -> concat optcom (fun _ -> concat (grpchoice ()) (fun q -> ret (CDDL_Spec_AST_Driver.mk_GConcat a q)))))
-    (ret GNop)
+    (concat (grpent ()) (fun a -> concat optcom (fun com -> concat (grpchoice ()) (fun q ->
+                                                              match q with
+                                                              | None -> ret (Some (
+                                                                                 match com with
+                                                                                 | None -> a
+                                                                                 | Some _ -> Thing (close_group a)
+                                                                          ))
+                                                              | Some q -> ret (Some (Thing (CDDL_Spec_AST_Driver.mk_GConcat (close_group a) (close_group q))))
+    ))))
+    (ret None)
 )
 
 and grpent () = debug "grpent" (
   choices
     [
       concat option_occur (fun f -> concat (option_memberkey ()) (fun g -> concat (type_ ()) (fun x -> ret (f (g x)))));
-      concat option_occur (fun f -> concat groupname (* option(genericarg) *) (fun (_, g) -> ret (f (GDef g))));
+      concat option_occur (fun f -> concat groupname (* option(genericarg) *) (fun (k, g) -> ret (f (match k with
+                                                                                                     | Regular -> Name g
+                                                                                                     | SocketGroup -> Thing (GDef g)
+                                                                                                     | _ -> failwith "grpent: groupname"
+        ))));
       concat option_occur (fun f -> concat lparen (fun _ -> concat s (fun _ -> concat (group ()) (fun g -> concat s (fun _ -> concat rparen (fun _ -> ret (f g)))))));
     ]
 )
@@ -366,15 +379,18 @@ and grpent () = debug "grpent" (
 and option_memberkey () = debug "option_memberkey" (
   choice
     (concat (memberkey ()) (fun x -> concat s (fun _ -> ret x)))
-    (ret (fun x -> GElem (false, TElem EAny, x)))
+    (ret (fun x -> match x with
+                   | Name s -> Name s
+                   | Thing t -> Thing (GElem (false, TElem EAny, close_typ x))
+    ))
 )
 
 and memberkey () = debug "memberkey" (
   choices
     [
-      concat (type1 ()) (fun key -> concat s (fun _ -> concat memberkey_cut (fun cut -> concat arrow (fun _ -> ret (fun x -> GElem (cut, key, x))))));
-      concat bareword (fun (k, key) -> if k <> Regular then fail else concat s (fun _ -> concat colon (fun _ -> ret (fun x -> GElem (true, TElem (ELiteral (LTextString (key))), x)))));
-      concat value (fun key -> concat s (fun _ -> concat colon (fun _ -> ret (fun x -> (GElem (true, TElem (ELiteral key), x))))));
+      concat (type1 ()) (fun key -> concat s (fun _ -> concat memberkey_cut (fun cut -> concat arrow (fun _ -> ret (fun x -> Thing (GElem (cut, close_typ key, close_typ x)))))));
+      concat bareword (fun (k, key) -> if k <> Regular then fail else concat s (fun _ -> concat colon (fun _ -> ret (fun x -> Thing (GElem (true, TElem (ELiteral (LTextString (key))), close_typ x))))));
+      concat value (fun key -> concat s (fun _ -> concat colon (fun _ -> ret (fun x -> Thing (GElem (true, TElem (ELiteral key), close_typ x))))));
     ]
 )
 
--- src/cddl/tool/ocaml/evercddl-lib/ParseFromFile.ml
+++ src/cddl/tool/ocaml/evercddl-lib/ParseFromFile.ml
@@ -24,30 +24,45 @@ let rec parse' env = function
      end
 
 let empty_env = {
-    CDDLParser.env = CDDLNameEnv.empty_name_env;
-    sockets = [];
+    CDDLParser.sockets = [];
     result = [];
 }
 
+let rec solve_names
+  (solved: (string * CDDL_Spec_AST_Base.decl) list)
+  (unknown: (string * CDDL_Spec_AST_Base.decl CDDLParser.thing_or_name) list)
+= function
+  | [] ->
+    begin match unknown with
+    | [] -> List.rev solved
+    | (a, CDDLParser.Name b) :: _ -> failwith ("solve_names: definition `" ^ a ^ " = " ^ b ^ "` has not been solved")
+    | (_, _) :: _ -> failwith ("solve_names: this should not happen")
+    end
+  | (a, CDDLParser.Thing d) :: q ->
+     solve_names ((a, d) :: solved) [] (List.rev_append unknown q)
+  | ((a, CDDLParser.Name n) as x) :: q ->
+    begin match List.assoc_opt n solved with
+    | Some (DType _) -> solve_names ((a, DType (TDef n)) :: solved) [] (List.rev_append unknown q)
+    | Some (DGroup _) -> solve_names ((a, DGroup (GDef n)) :: solved) [] (List.rev_append unknown q)
+    | None -> solve_names solved (x :: unknown) q
+    end
+
 let plug_empty_socket
-  (st: CDDLParser.state)
   (accu: (string * CDDL_Spec_AST_Base.decl) list)
-  (name: string)
-= if List.mem_assoc name accu
+  (name: (string * CDDLParser.id_kind))
+= let (n, k) = name in
+  if List.mem_assoc n accu
   then accu
-  else
-    let def = match CDDLNameEnv.cddl_env_of_env st.env name with
-    | Some CDDL_Spec_AST_Base.NType -> CDDL_Spec_AST_Base.DType (CDDL_Spec_AST_Base.TElem CDDL_Spec_AST_Base.EAlwaysFalse)
-    | Some CDDL_Spec_AST_Base.NGroup -> CDDL_Spec_AST_Base.DGroup CDDL_Spec_AST_Base.GAlwaysFalse
-    | _ -> failwith "plug_empty_socket: this should not happen. Please report"
-    in
-    (name, def) :: accu
+  else match k with
+  | Regular -> accu
+  | SocketType -> (n, CDDL_Spec_AST_Base.DType (CDDL_Spec_AST_Base.TElem CDDL_Spec_AST_Base.EAlwaysFalse)) :: accu
+  | SocketGroup -> (n, CDDL_Spec_AST_Base.DGroup CDDL_Spec_AST_Base.GAlwaysFalse) :: accu
 
 let plug_empty_sockets
   (st: CDDLParser.state)
   (l: (string * CDDL_Spec_AST_Base.decl) list)
-= List.fold_left (plug_empty_socket st) l st.sockets
+= List.fold_left (plug_empty_socket) l st.sockets
 
 let parse_from_files l = match parse' empty_env l with
   | None -> None
-  | Some env -> Some (plug_empty_sockets env env.result)
+  | Some env -> Some (plug_empty_sockets env (solve_names [] [] env.result))
