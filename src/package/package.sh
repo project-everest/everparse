cca03f7798e5a70157276e20d66165e93e6a0c32
Tahina Ramananandro <taramana@microsoft.com>
2026-06-25 11:24:36 -0700

    select the x86_64 libgmp10.dll

--- src/package/package.sh
+++ src/package/package.sh
@@ -107,7 +107,7 @@ make_everparse() {
 	# use (Cygwin's) `ldd` to find out which dll we are using exactly, and
 	# ship that.
 	# (See FStarLang/FStar#4064)
-	LIBGMP10_DLL="$(ldd "$FSTAR_EXE" | sed -n 's/^[[:space:]]libgmp-10.dll => *\([^ ]*\) .*$/\1/p')"
+	LIBGMP10_DLL="$(ldd "$FSTAR_EXE" | grep x86_64 | sed -n 's/^[[:space:]]libgmp-10.dll => *\([^ ]*\) .*$/\1/p')"
 	if [[ -z "$LIBGMP10_DLL" ]] ; then
             echo libgmp-10.dll is missing
             exit 1
