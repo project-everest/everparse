c5ffcc27acc03e5829c1258e81894000766c7b38
Tahina Ramananandro <taramana@microsoft.com>
2026-07-01 14:45:15 -0700

    (Low* only) cddl-tool: unify uses of krml_lib

--- src/cddl/tool/ocaml/evercddl-gen/Main.ml
+++ src/cddl/tool/ocaml/evercddl-gen/Main.ml
@@ -179,7 +179,7 @@ let everparse_src_cddl_tool = Filename.concat everparse_src_cddl "tool"
 let everparse_home_lib = Filename.concat everparse_home "lib"
 let everparse_home_lib_evercddl = Filename.concat everparse_home_lib "evercddl"
 
-let include_options =
+let include_options krml_home_krmllib =
   include_option_of_paths
     [
       everparse_src_cbor_spec;
@@ -213,7 +213,7 @@ let include_options_for_rust =
 (* TODO: honor OTHERFLAGS. This would require implementing Bash word
    splitting, since we are using lists of arguments. *)
 
-let fstar_options =
+let fstar_options krmllib =
   z3_executable_option @
     [
       "--cache_checked_modules";
@@ -221,7 +221,7 @@ let fstar_options =
       "--cmi";
       "--ext"; "context_pruning";
     ] @
-      include_options
+      include_options krmllib
 
 let admit = ref false
 
@@ -289,6 +289,7 @@ let _ =
   let basename = produce_fst_file tmpdir dir in
   if !fstar_only then exit 0;
   let filename = Filename.concat dir basename in
+  let krmllib = krmllib tmpdir in
   let res = run_cmd fstar_exe
     (
       [
@@ -297,7 +298,7 @@ let _ =
         "--already_cached"; ("*,-" ^ !mname);
       ] @
         (if !admit then [ "--admit_smt_queries"; "true" ] else []) @
-        fstar_options
+        fstar_options krmllib
     )
   in
   if res <> 0
@@ -318,7 +319,7 @@ let _ =
           "--extract_module"; !mname;
         ] @
           (if is_rust () then include_options_for_rust else []) @
-          fstar_options
+          fstar_options krmllib
       )
   in
   if res <> 0
@@ -387,7 +388,6 @@ let _ =
       else begin
         let cc = try Sys.getenv "CC" with Not_found -> "cc" in
         let det_c = Filename.concat (Filename.concat everparse_src_cbor_pulse "det") "c" in
-        let krmllib = krmllib tmpdir in
         let krmlinclude = krmlinclude tmpdir in
         let cc_args = [
           "-I"; Filename.concat (Filename.concat krmllib "dist") "minimal";
