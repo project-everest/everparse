020212959cf52ca0c9a7a53365c241c218399d2f
Tahina Ramananandro <taramana@microsoft.com>
2026-07-02 19:31:58 -0700

    Revert "(Low* only) add krmllib to F* include path"
    This reverts commit d612f1fb60f9b48836954e97f7be8289d33498fc.

--- fstar.sh
+++ fstar.sh
@@ -3,10 +3,4 @@ set -e
 unset CDPATH
 EVERPARSE_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$EVERPARSE_HOME"/env.sh
-if [[ -z "$KRML_LIB" ]] ; then
-    KRML_LIB="$("$KRML_EXE" -locate-krmllib)"
-fi
-if [[ "$OS" = Windows_NT ]] ; then
-    KRML_LIB="$(cygpath -m "$(echo "$KRML_LIB" | sed 's!\r!!g')")"
-fi
-exec "$FSTAR_EXE" --z3version $EVERPARSE_Z3_VERSION --include "$KRML_LIB" --include "$KRML_LIB"/obj --include "$PULSE_HOME/lib/pulse" "$@"
+exec "$FSTAR_EXE" --z3version $EVERPARSE_Z3_VERSION --include "$KRML_HOME/krmllib" --include "$KRML_HOME/krmllib/obj" --include "$PULSE_HOME/lib/pulse" "$@"
