6a1fe096f2e93bd9d58552c92fd629676599e599
Tahina Ramananandro <taramana@microsoft.com>
2026-07-02 19:30:26 -0700

    Revert "(master only) 3d: karamel has its own opt path"
    This reverts commit c6423d15771c83ce20add6fdb79f22d5ad87c5c9.

--- src/3d/ocaml/Batch.ml
+++ src/3d/ocaml/Batch.ml
@@ -8,7 +8,7 @@ let krml =
   with
   | Not_found ->
     let krml = "krml" ^ (if Sys.win32 then ".exe" else "") in
-    let opt_krml = Filename.concat (Filename.concat (Filename.concat (Filename.concat (Filename.concat everparse_home "opt") "karamel") "out") "bin") krml in
+    let opt_krml = Filename.concat (Filename.concat (Filename.concat (Filename.concat (Filename.concat (Filename.concat everparse_home "opt") "FStar") "karamel") "out") "bin") krml in
     if Sys.file_exists opt_krml
     then opt_krml
     else
