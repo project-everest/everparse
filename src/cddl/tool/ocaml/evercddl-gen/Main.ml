575439ad2fa89dc5e10e8f3a45aeecd4739bdf53
Tahina Ramananandro <taramana@microsoft.com>
2026-07-02 19:16:21 -0700

    Sys.cygwin -> Sys.win32

--- src/cddl/tool/ocaml/evercddl-gen/Main.ml
+++ src/cddl/tool/ocaml/evercddl-gen/Main.ml
@@ -84,7 +84,7 @@ let string_matches_regexp s r = Re.execp r s
 let filename_is_relative n =
   if Filename.is_relative n
   then true
-  else if Sys.cygwin
+  else if Sys.win32
   then not (string_matches_regexp n regexp_path_drive)
   else false
 
@@ -121,7 +121,7 @@ let krml_exe =
     Sys.getenv "KRML_EXE"
   with
   | Not_found ->
-    let krml = "krml" ^ (if Sys.cygwin then ".exe" else "") in
+    let krml = "krml" ^ (if Sys.win32 then ".exe" else "") in
     let opt_krml = Filename.concat (Filename.concat (Filename.concat (Filename.concat (Filename.concat everparse_home "opt") "karamel") "out") "bin") krml in
     if Sys.file_exists opt_krml
     then opt_krml
